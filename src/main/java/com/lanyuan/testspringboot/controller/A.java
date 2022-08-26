e73b5191ad00e0f0e0e41ab73b73b19b28290137
梦魔 <3311515802@qq.com>
2022-08-26 19:30:22 +0800

    commit

--- src/main/java/com/lanyuan/testspringboot/controller/A.java
+++ src/main/java/com/lanyuan/testspringboot/controller/A.java
@@ -5,5 +5,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("你好啊，这是测试");
         System.out.println();
+        System.out.println("你好啊,这也是测试");
     }
 }
