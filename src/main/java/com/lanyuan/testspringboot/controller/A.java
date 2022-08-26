a61e8677af8c63df8f8890375d87d9321112bc37
hjm <wy17396433052@163.com>
2022-08-26 19:15:21 +0800

    tenth Commit

--- src/main/java/com/lanyuan/testspringboot/controller/A.java
+++ src/main/java/com/lanyuan/testspringboot/controller/A.java
@@ -4,5 +4,6 @@ package com.lanyuan.testspringboot.controller;
 public class A {
     public static void main(String[] args) {
         System.out.println("你好啊，这是测试");
+        System.out.println();
     }
 }
